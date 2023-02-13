1dd9a4ba703da0c6ae7109f85487b2d85b22c3da
Oussama ELJabbari <contact@ojpro.me>
2023-02-13 10:31:58 +0000

    minor edits

--- components/Cards/quote.js
+++ components/Cards/quote.js
@@ -7,8 +7,8 @@ export default function QuoteCard(){
             <div className="text-white w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 h-min p-4 relative">
                 <RiDoubleQuotesL className="text-2xl mr-auto text-green-400"/>
                 <div className="p-8">
-                    <p className=" font-quote text-2xl font-semibold">The best thing about the future is that it only comes one day at a time.</p>
-                    <snap className="block mt-6 font-author font-light text-xs uppercase"> <span className="font-bold text-green-400 text-lg">-</span> Abraham Lincoln</snap>
+                    <p className=" font-quote text-2xl lg:text-3xl font-bold">The best thing about the future is that it only comes one day at a time.</p>
+                    <snap className="block mt-6 font-author font-light text-xs uppercase"> <span className="text-green-400 text-xs">-</span> Abraham Lincoln</snap>
                 </div>
                 <RiDoubleQuotesR className="text-2xl ml-auto -mt-6 text-green-400"/>
 
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600&family=Varela+Round&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600;700&family=Varela+Round&display=swap" rel="stylesheet" />
 
       <title>QuoteZen - Web App</title>
       </Head>
