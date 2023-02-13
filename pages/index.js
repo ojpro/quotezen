20c5bcdb8ef828954b936e1b65cf3baaf1739285
Oussama ELJabbari <contact@ojpro.me>
2023-02-13 10:22:04 +0000

    ✨ create home page interface.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Head from "next/head"
+import QuoteCard from "@/components/Cards/quote"
+
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">
-      QuoteZen
-    </h1>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600&family=Varela+Round&display=swap" rel="stylesheet" />
+
+      <title>QuoteZen - Web App</title>
+      </Head>
+      <QuoteCard></QuoteCard>
+    </>
   )
 }
