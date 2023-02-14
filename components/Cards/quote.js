d703286b6c49d141bcb61e2cc848f19a3cebcd48
Oussama ELJabbari <contact@ojpro.me>
2023-02-14 08:01:27 +0000

    added re-generate button functionality

--- components/Cards/quote.js
+++ components/Cards/quote.js
@@ -2,27 +2,34 @@ import { RiDoubleQuotesR, RiDoubleQuotesL } from "react-icons/ri"
 import { BsDice5 } from "react-icons/bs"
 import { useState, useEffect } from "react";
 
+// TODO: remove axios  
+
+function useRandomQuote() {
+    const [quote, setQuote] = useState({});
+  
+    function fetchQuote() {
+      fetch('https://quotezen.vercel.app/api/quotes/random')
+        .then((response) => response.json())
+        .then((data) => {
+          setQuote(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  
+    useEffect(() => {
+      fetchQuote();
+    }, []);
+  
+    return [quote, fetchQuote];
+  }
+  
+
 export default function QuoteCard() {
 
-    const [quote, setQuote] = useState({})
-
-    useEffect(() => {
-        let firstFetch = true
-
-        if (firstFetch) {
-            fetch('https://quotezen.vercel.app/api/quotes/random')
-                .then((response) => response.json())
-                .then((data) => {
-                    setQuote(data)
-                    firstFetch = false
-                })
-                .catch((err) => {
-                    console.log(err.message);
-                });
-        }
-    }, []);
-
-
+    const [quote, fetchQuote] = useRandomQuote();
+    
     return (
         <div className="min-w-full sm:min bg-[#212F31] min-h-screen flex flex-row justify-center items-center">
             <div className="text-white w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 h-min p-4 relative">
@@ -33,7 +40,7 @@ export default function QuoteCard() {
                 </div>
                 <RiDoubleQuotesR className="text-2xl ml-auto -mt-6 text-green-400" />
 
-                <div className="p-4 mt-4 bg-gray-100/10 mx-auto max-w-min rounded-full shadow cursor-pointer hover:-rotate-6">
+                <div id="generateBtn" onClick={fetchQuote} className="p-4 mt-4 bg-gray-100/10 mx-auto max-w-min rounded-full shadow cursor-pointer hover:-rotate-6">
                     <BsDice5 className="text-xl rotate-6" />
                 </div>
             </div>
